f432ea30112f6fa10b34b92ecf97421e2aa655f9
Allen Chung <allen.chung@pvelocity.com>
2017-11-09 11:50:49 -0500

    added new round function for precision rounding

--- PV.js
+++ PV.js
@@ -68,6 +68,22 @@ var root = (typeof window === 'undefined') ? global : window;
 })(root);
 
 (function(root) {
+    root.PV.round = function(value, exp) {
+        //https://stackoverflow.com/questions/1726630/formatting-a-number-with-exactly-two-decimals-in-javascript
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round#Example:_Decimal_rounding
+        if (typeof exp === 'undefined' || +exp === 0)
+            return Math.round(value);
+        value = +value;
+        exp = +exp;
+        if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0))
+            return NaN;
+        // Shift
+        value = value.toString().split('e');
+        value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp)));
+        // Shift back
+        value = value.toString().split('e');
+        return +(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp));
+    };
     root.PV.randomInt = function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
