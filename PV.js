pVelocity/pvjs

commit 032f887372504fcd3079701603367160126da171

Remove whitespaces before parseFloat in convertStrToFloat

PV.js
@@ -333,7 +333,7 @@ var root = (typeof window === 'undefined') ? global : window;
             defaultNum = defaultFloat;
         }
 
-        var num = parseFloat(string);
+        var num = parseFloat(string.replace(/ /g, ''));
         if (PV.isNumber(num)) {
             return num;
         } else {

commit c767d58567d17b6cd7d4a3be48605d6dbc76cec3

Fixed a regression from commit 032f887

PV.js
@@ -332,8 +332,12 @@ var root = (typeof window === 'undefined') ? global : window;
         if (PV.isNumber(defaultFloat)) {
             defaultNum = defaultFloat;
         }
-
-        var num = parseFloat(string.replace(/[ ,]/g, ''));
+        var num = 0;
+        if (PV.isString(string)) {
+            num = parseFloat(string.replace(/[ ,]/g, ''));
+        } else {
+            num = parseFloat(string);
+        }
         if (PV.isNumber(num)) {
             return num;
         } else {